9ba1d8586b8621220073b4c2681175eaf67cd3ee
alexanderkoleshko <koleshko.a@gmail.com>
2020-05-16 18:23:38 +0300

    Fix 7lab

---
+++ lab7/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace lab7
+{
+    class Program
+    {
+        static void Main()
+        {
+            RationalNum first = new RationalNum(1,6);
+            RationalNum second = new RationalNum(4, 8);
+
+            List<RationalNum> numbers = new List<RationalNum>();
+            numbers.Add(first);
+            numbers.Add(second);
+
+            
+
+            while (true)
+            {
+                Console.WriteLine("1 - Add new number\n2 - View number\n3 - Math operations\n4 - Compare\n5 - Simplify all numbers\n6 - Exit");
+
+                switch (Console.ReadKey(false).Key)
+                {
+                    case ConsoleKey.D1: AddNumbers(numbers); Console.Clear(); break;
+                    case ConsoleKey.D2: ViewNumbers(numbers); Console.Clear(); break;
+                    case ConsoleKey.D3: Calculate(numbers); Console.Clear(); break;
+                    case ConsoleKey.D4: NumComparer(numbers); Console.Clear(); break;
+                    case ConsoleKey.D5: AllSimple(numbers); break;
+                    case ConsoleKey.D6: return;
+                }
+            }
+
+        }
+
+        static public void AddNumbers(List<RationalNum> nums)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Choose the format of input number : \n1 - Numerator/Denominator\n2 - Numerator.Denominator");
+                switch (Console.ReadKey(false).Key)
+                {
+                    case ConsoleKey.D1: Console.Clear(); Console.WriteLine("Input\n"); nums.Add(RationalNum.ConvertToRat(Console.ReadLine(), "n")); return;
+                    case ConsoleKey.D2: Console.Clear(); Console.WriteLine("Input\n"); nums.Add(RationalNum.ConvertToRat(Console.ReadLine(), "s")); return;
+                    default: Console.WriteLine("Incorrect input"); break;
+                }
+                continue;
+            }
+        }
+
+        static public void ViewNumbers(List<RationalNum> nums)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Choose the format of the output : \n1 - Numerator/Denominator\n2 - Numerator.Denominator\n3 - Proper view ");
+                switch (Console.ReadKey(false).Key)
+                {
+                    case ConsoleKey.D1: 
+                                    Console.Clear(); 
+                                    for (int i = 0; i < nums.Count; i++) 
+                                    Console.WriteLine(nums[i].ToString("n")); Console.ReadKey();
+                                    return;
+
+                    case ConsoleKey.D2:
+                                    Console.Clear();
+                                    for (int i = 0; i < nums.Count; i++)
+                                    Console.WriteLine(nums[i].ToString("s")); Console.ReadKey();
+                                    return;
+                    
+                    case ConsoleKey.D3:
+                                    Console.Clear();
+                                    for (int i = 0; i < nums.Count; i++)
+                                    Console.WriteLine(nums[i].ToString("p")); Console.ReadKey();
+                                    return;
+
+                    default: Console.WriteLine("Incorrect input"); break;
+                }
+                continue;
+            }
+
+        }
+
+        static public void Calculate(List<RationalNum> nums)
+        {
+            RationalNum first, second, result ;
+            while (true)
+            {
+                NumChoice(nums,out first,out second);
+
+                Console.Clear();
+                Console.WriteLine("Choose the operation : \n1 - Sum\n2 - Subtraction\n3 - Multiplication\n4 - Division\n5 - Exit");
+                switch (Console.ReadKey(false).Key)
+                {
+                    case ConsoleKey.D1: 
+                        result = first + second; result.Simplify();
+                        Console.Clear();
+                        Console.WriteLine(first.ToString("n") + " + " + second.ToString("n") + " = " + result.ToString("n")); Console.ReadKey();
+                        return;
+
+                    case ConsoleKey.D2:
+                        result = first - second; result.Simplify();
+                        Console.Clear();
+                        Console.WriteLine(first.ToString("n") + " - " + second.ToString("n") + " = " + result.ToString("n")); Console.ReadKey();
+                        return;
+
+                    case ConsoleKey.D3:
+                        result = first * second; result.Simplify();
+                        Console.Clear();
+                        Console.WriteLine(first.ToString("n") + " * " + second.ToString("n") + " = " + result.ToString("n")); Console.ReadKey();
+                        return;
+                    case ConsoleKey.D4:
+                        result = first / second; result.Simplify();
+                        Console.Clear();
+                        Console.WriteLine(first.ToString("n") + " / " + second.ToString("n") + " = " + result.ToString("n")); Console.ReadKey();
+                        return;
+                    case ConsoleKey.D5: return;
+
+
+                    default: Console.WriteLine("Incorrect input"); break;
+                }
+                continue;
+            }
+
+        }
+
+        static void NumComparer(List<RationalNum> nums)
+        {
+            NumChoice(nums, out RationalNum first, out RationalNum second);
+
+            if (first > second) {
+                Console.WriteLine(first.ToString("n") + " > " + second.ToString("n"));
+                Console.ReadKey();
+                return;
+            }
+            
+            if (first < second)
+            {
+                Console.WriteLine(first.ToString("n") + " < " + second.ToString("n"));
+                Console.ReadKey();
+                return;
+            }
+            
+            if (first == second)
+            {
+                Console.WriteLine(first.ToString("n") + " = " + second.ToString("n"));
+                Console.ReadKey();
+                return;
+            }
+        }
+
+        static void AllSimple(List<RationalNum> nums)
+        {
+            for (int i = 0; i < nums.Count; i++)
+            {
+                 nums[i].Simplify();
+            }
+
+            Console.Clear();
+        }
+
+        static void NumChoice(List<RationalNum> nums, out RationalNum first, out RationalNum second)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Choose 2 numbers by their id");
+                for (int i = 0; i < nums.Count; i++)
+                {
+
+                    Console.WriteLine(i + ") " + nums[i].ToString("n"));
+                }
+
+                Console.WriteLine("First id : ");
+                if (!int.TryParse(Console.ReadLine(), out int index))
+                    continue;
+
+                    if (index >= nums.Count) continue;
+
+                    first = nums[index];
+
+                Console.WriteLine("Second id : ");
+                if (!int.TryParse(Console.ReadLine(), out index))
+                    continue;
+                if (index >= nums.Count) continue;
+
+                second = nums[index];
+                return;
+
+
+            }
+
+
+        }
+
+
+
+
+    }
+}
---
+++ lab7/RationalNum.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace lab7
+{
+    class RationalNum
+    {
+        int  Denominator { get; set; }
+        int Numerator { get; set; }
+        public RationalNum(int numerator, int denominator)
+        {
+            if (numerator < 0 && denominator < 0)
+            {
+                numerator = Math.Abs(numerator);
+                denominator = Math.Abs(denominator);
+            }
+            else if (numerator > 0 && denominator < 0)
+            {
+                numerator = -(numerator);
+                denominator = Math.Abs(denominator);
+            }
+
+            if (denominator == 0)
+            {
+                Console.WriteLine("Error, division by zero");
+
+            }
+
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public RationalNum(int numerator)
+        {
+            Numerator = numerator;
+            Denominator = 1;
+        }
+
+        public RationalNum(double number)
+        {
+            int drob;
+
+            Numerator = (int)number;
+
+            drob = (int)(number - Numerator)*100;
+            Numerator *= 100;
+
+            Denominator = drob;
+        }
+
+        public int Gcd(int a, int b)
+        {
+            while (b != 0)
+                b = a % (a = b);
+            return Math.Abs(a);
+        }
+
+        public void Simplify()
+        {
+            int gcd = Gcd(Numerator, Denominator);
+
+            if (gcd == 0)
+                gcd = 1;
+            
+
+            Numerator /= gcd;
+            Denominator /= gcd;
+        }
+
+        static public RationalNum operator+(RationalNum first, RationalNum second)
+        {
+            return new RationalNum(first.Numerator * second.Denominator + second.Numerator * first.Denominator, first.Denominator * second.Denominator);
+        }
+
+        static public RationalNum operator -(RationalNum first, RationalNum second)
+        {
+            return new RationalNum(first.Numerator * second.Denominator - second.Numerator * first.Denominator, first.Denominator * second.Denominator);
+        }
+
+        static public RationalNum operator *(RationalNum first, RationalNum second)
+        {
+            return new RationalNum(first.Numerator * second.Numerator, first.Denominator * second.Denominator);
+        }
+
+        static public RationalNum operator /(RationalNum first, RationalNum second)
+        {
+            if (second.Denominator == 0) return new RationalNum(1);
+            return new RationalNum(first.Numerator * second.Denominator, first.Denominator * second.Numerator);
+        }
+
+        static public bool operator >(RationalNum first, RationalNum second)
+        {
+            return first.Numerator * second.Denominator > second.Numerator * first.Denominator;
+        }
+
+        static public bool operator <(RationalNum first, RationalNum second)
+        {
+            return first.Numerator * second.Denominator < second.Numerator * first.Denominator;
+        }
+
+        static public bool operator <=(RationalNum first, RationalNum second)
+        {
+            return first.Numerator * second.Denominator <= second.Numerator * first.Denominator;
+        }
+
+        static public bool operator >=(RationalNum first, RationalNum second)
+        {
+            return first.Numerator * second.Denominator >= second.Numerator * first.Denominator;
+        }
+
+        static public bool operator ==(RationalNum first, RationalNum second)
+        {
+            return first.Numerator * second.Denominator == second.Numerator * first.Denominator;
+        }
+
+        static public bool operator !=(RationalNum first, RationalNum second)
+        {
+            return first.Numerator * second.Denominator != second.Numerator * first.Denominator;
+        }
+
+
+
+        static public implicit operator RationalNum(int number)
+        {
+            return new RationalNum(number);
+        }
+
+        static public explicit operator int(RationalNum number)
+        {
+            return (number.Numerator / number.Denominator);
+        }
+
+        static public explicit operator double(RationalNum number)
+        {
+            return (number.Numerator / number.Denominator);
+        }
+
+        static public implicit operator RationalNum(double number)
+        {
+            return new RationalNum((int)number * 10000 ,10000);
+        }
+
+
+        public string ToString(string form)
+        {
+
+            switch (form)
+            {
+                case "n": return string.Concat(Numerator , '/' , Denominator); 
+                case "p":
+
+                    if (Numerator <= Denominator && Numerator > 0)
+                        return string.Concat(Numerator, '/', Denominator);
+                    else
+                        if (Numerator % Denominator == 0) return string.Concat(Numerator / Denominator);
+                    else
+                            return string.Concat(Numerator / Denominator,' ', Math.Abs(Numerator) % Denominator, '/',Denominator);
+                    
+                case "s": return string.Concat((double)Numerator / Denominator);
+                default: return string.Concat(Numerator , '/' , Denominator);
+            }
+        }
+
+        public override string ToString()
+        {
+             return string.Concat(Numerator , '/' , Denominator);
+        }
+
+       static public RationalNum ConvertToRat(string stroka, string format)
+        {
+            stroka.Trim();
+
+            if (int.TryParse(stroka, out int res))
+                return new RationalNum(res);
+            
+            string[] nums = stroka.Split('/','.',',');
+
+            if (!int.TryParse(nums[0], out int first) || !int.TryParse(nums[1], out int second))
+            {
+                Console.WriteLine("Incorrect input will be 1/1, try again"); Thread.Sleep(500); return new RationalNum(1);
+            }
+
+
+
+                switch (format)
+            {
+                case "n": 
+                    return new RationalNum(first, second);
+
+                case "s":  return new RationalNum(first * (int)Math.Pow(10, nums[1].Length) + second, (int)Math.Pow(10,nums[1].Length));
+
+
+                default: Console.WriteLine("Incorrect input will be 1/1, try again"); Thread.Sleep(100);  return new RationalNum(1);
+            }
+
+        }
+
+
+
+
+
+    }
+}
